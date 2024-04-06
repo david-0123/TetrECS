c1efdbcdd9bfd88d8e143bc713062cf68167d92c
David <88691455+david-0123@users.noreply.github.com>
2024-04-06 19:06:25 +0100

    Modify canPlayPiece method to catch IndexOutOfBoundsException when iterating over a piece array

--- src/main/java/uk/ac/soton/comp1206/game/Grid.java
+++ src/main/java/uk/ac/soton/comp1206/game/Grid.java
@@ -117,7 +117,13 @@ public class Grid {
 
         for (int i = 0; i < 3; i++) { // Columns
             for (int j = 0; j < 3; j++) { // Rows
-                if ((piece.getBlocks()[i][j] > 0) && (grid[i+relativeX][j+relativeY].get() > 1)) return false;
+                if (piece.getBlocks()[i][j] > 0) {
+                    try {
+                        if (grid[i+relativeX][j+relativeY].get() > 1) return false;
+                    } catch (IndexOutOfBoundsException e) {
+                        return false;
+                    }
+                }
             }
         }
 
@@ -139,7 +145,7 @@ public class Grid {
                 }
             }
 
-            logger.info("Played {}:{}", piece, piece.toString());
+            logger.info("Played {}: {}", piece, piece.toString());
         }
     }
 }
